8aa0ee2eddaf459294859e9c368825ad5e02d7a0
Richard Annand <rj170590@gmail.com>
2020-11-23 02:08:34 +0000

    test: added workflow for testing

--- tests/integrations/main.tf
+++ tests/integrations/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region  = "eu-west-2"
   version = "2.33.0"
 }
+
+terraform {
+  backend "s3" {
+    bucket  = "systemsmystery-terraform-testing-statefiles"
+    key     = "terraform-s3-backup-bucket/terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = "true"
+  }
+}
 resource "aws_route53_zone" "route53_zone" {
   name = "testing.systemsmystery.tech"
 }
